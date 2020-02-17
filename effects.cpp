421bc52d7693b9bc9dea2ecc75510828f90c6db1
Arman <arm.nov@ya.ru>
2020-02-17 14:57:50 +0300

    make_pale used for make_zombie

--- effects.cpp
+++ effects.cpp
@@ -2,10 +2,11 @@
 #include "msestimator.h"
 #include <cstdlib>
 #include <vector>
+#include <iostream>
 
 void make_zombie(cv::Mat& face)
 {
-    MSEstimator estimator;
+    MSEstimator estimator("make_zombie done, ");
     auto it = face.begin<cv::Vec3b>();
     const auto end = face.end<cv::Vec3b>();
     const std::vector<cv::Scalar> zombie_colors {
@@ -18,9 +19,10 @@ void make_zombie(cv::Mat& face)
         {0x83, 0xb2, 0x2f},
     };
 
-    std::srand(unsigned(std::time(0)));
+    std::srand(unsigned(std::time(nullptr)));
      //uchar b,g,r;
     //cv::bitwise_not(face, face);
+    make_pale(face);
     for (; it != end; ++it) {
         auto& b = (*it)[0];
         auto& g = (*it)[1];
@@ -28,17 +30,27 @@ void make_zombie(cv::Mat& face)
 
         const uchar avr = (b + g + r) / 3;
 
-
-        g += g < 200? 50: 250 - g;
+        //g += g < 200? 50: 250 - g;
         if (r > 150 && g < 180 && b < 100) {
             r = b = g = avr;
         }
 
         int rand_val = std::rand();
-        if (rand_val%3==0) {
+        if (rand_val%2==0) {
             b = zombie_colors[rand_val%zombie_colors.size()][2];
             r = zombie_colors[rand_val%zombie_colors.size()][0];
         }
         //r *= 0.3;
     }
+}
+
+void make_pale(cv::Mat& face, double gamma)
+{
+    cv::Mat lookUpTable(1, 256, CV_8U);
+    uchar* p = lookUpTable.ptr();
+
+    for( int i = 0; i < 256; ++i)
+        p[i] = cv::saturate_cast<uchar>(pow(i / 255.0, gamma) * 255.0);
+
+   cv::LUT(face, lookUpTable, face);
 }
--- effects.h
+++ effects.h
@@ -2,3 +2,4 @@
 #include "opencv2/imgproc.hpp"
 
 void make_zombie(cv::Mat& face);
+void make_pale(cv::Mat& face, double gamma = 0.3);
