aa5904b73718d61bafaa1738ccbb73738d65f9d5
arman-novikov <arm.nov@ya.ru>
2020-02-24 19:04:03 +0300

    getFaceAttr method added

--- face_recognizer.cpp
+++ face_recognizer.cpp
@@ -43,3 +43,53 @@ std::cerr << std::endl;
     }
     return res;
 }
+
+/*
+ * @warning no preparing work done
+*/
+
+void FaceRecognizer::getFaceAttr(
+        const cv::Mat& frame, std::vector<FaceAttr>& res,
+        double factor)
+{
+    MSEstimator estimator("getFaceAttr done, ");
+    std::vector<cv::Rect> faces{}, eyes{};
+    cv::Mat prepared_frame;
+
+    cv::cvtColor(frame, prepared_frame, cv::COLOR_BGR2GRAY);
+    cv::resize(prepared_frame, prepared_frame,
+               cv::Size(), factor, factor, cv::INTER_LINEAR);
+    cv::equalizeHist(prepared_frame, prepared_frame);
+
+    // @todo: use 3 for @param minNeighbors
+    // @todo: check _face_cascade on emptiness
+    this->_face_cascade.detectMultiScale(
+        prepared_frame, faces,
+        1.1, 2, cv::CASCADE_SCALE_IMAGE, cv::Size(250,250));
+
+
+    for (const auto& face: faces) {
+        const int x = static_cast<int>(face.tl().x/factor);
+        const int y = static_cast<int>(face.tl().y/factor);
+        const int width = static_cast<int>(face.width/factor);
+        const int height = static_cast<int>(face.height/factor);
+        FaceAttr fattr;
+        cv::Rect face_area_on_orig_frame(x, y, width, height);
+        fattr.face = frame(face_area_on_orig_frame);
+
+        this->_eyes_cascade.detectMultiScale(
+            prepared_frame, eyes,
+            1.1, 2, cv::CASCADE_SCALE_IMAGE, cv::Size(30,30)
+        );
+
+        for (const auto &eye: eyes) {
+            const int x = static_cast<int>(eye.tl().x/factor);
+            const int y = static_cast<int>(eye.tl().y/factor);
+            const int width = static_cast<int>(eye.width/factor);
+            const int height = static_cast<int>(eye.height/factor);
+            cv::Rect eye_area_on_orig_frame(x, y, width, height);
+            fattr.eyes.push_back(frame(eye_area_on_orig_frame));
+        }
+        res.push_back(fattr);
+    }
+}
--- face_recognizer.h
+++ face_recognizer.h
@@ -7,11 +7,20 @@
 #include <string>
 
 using mats_vec_t = std::vector<cv::Mat>;
+
+struct FaceAttr {
+    cv::Mat face;
+    mats_vec_t eyes;
+};
+
 class FaceRecognizer {
 public:
     FaceRecognizer(const std::string& haarcascade_face,
                    const std::string& haarcascade_eyes);
     mats_vec_t getFaceROI(const cv::Mat& frame, double factor = 1.0);
+    void getFaceAttr(
+            const cv::Mat& frame, std::vector<FaceAttr>& res,
+            double factor = 1.0);
 protected:
     cv::CascadeClassifier _face_cascade;
     cv::CascadeClassifier _eyes_cascade;
