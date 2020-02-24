802aa623714c50de0dee25344d01920a2b0ad19e
arman-novikov <arm.nov@ya.ru>
2020-02-24 16:07:58 +0300

    make_zombie() cleaned, mat_fill added

--- effects.cpp
+++ effects.cpp
@@ -4,44 +4,22 @@
 #include <vector>
 #include <iostream>
 
+void mat_fill(cv::Mat& mat, const cv::Vec3b& color)
+{
+    auto it = mat.begin<cv::Vec3b>();
+    const auto end = mat.end<cv::Vec3b>();
+
+    for (;it < end; ++it)
+        *it = color;
+}
+
+/*
+* @warning function is not finished
+*/
 void make_zombie(cv::Mat& face)
 {
     MSEstimator estimator("make_zombie done, ");
-    auto it = face.begin<cv::Vec3b>();
-    const auto end = face.end<cv::Vec3b>();
-    const std::vector<cv::Scalar> zombie_colors {
-        {0xc6, 0xbf, 0x2d},
-        {0xb5, 0xaf, 0x37},
-        {0xab, 0xa3, 0x11},
-        {0xc6, 0xbf, 0x2d},
-        {0xbf, 0xb8, 0x3d},
-        {0xa7, 0xd3, 0x58},
-        {0x83, 0xb2, 0x2f},
-    };
-
-    std::srand(unsigned(std::time(nullptr)));
-     //uchar b,g,r;
-    //cv::bitwise_not(face, face);
     make_pale(face);
-    for (; it != end; ++it) {
-        auto& b = (*it)[0];
-        auto& g = (*it)[1];
-        auto& r = (*it)[2];
-
-        const uchar avr = (b + g + r) / 3;
-
-        //g += g < 200? 50: 250 - g;
-        if (r > 150 && g < 180 && b < 100) {
-            r = b = g = avr;
-        }
-
-        int rand_val = std::rand();
-        if (rand_val%2==0) {
-            b = zombie_colors[rand_val%zombie_colors.size()][2];
-            r = zombie_colors[rand_val%zombie_colors.size()][0];
-        }
-        //r *= 0.3;
-    }
 }
 
 void make_pale(cv::Mat& face, double gamma)
--- effects.h
+++ effects.h
@@ -1,5 +1,6 @@
 #pragma once
 #include "opencv2/imgproc.hpp"
 
+void mat_fill(cv::Mat& mat, const cv::Vec3b& color = {0,255,0});
 void make_zombie(cv::Mat& face);
 void make_pale(cv::Mat& face, double gamma = 0.3);
