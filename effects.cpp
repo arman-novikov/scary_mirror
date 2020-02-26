53141a93bdbecb4a4585959946240c79c357e04d
arman-novikov <arm.nov@ya.ru>
2020-02-26 16:24:52 +0300

    make zombie boosted

--- effects.cpp
+++ effects.cpp
@@ -16,10 +16,26 @@ void mat_fill(cv::Mat& mat, const cv::Vec3b& color)
 /*
 * @warning function is not finished
 */
-void make_zombie(cv::Mat& face)
+void make_zombie(cv::Mat& orig_face, cv::Mat zombie_face)
 {
     MSEstimator estimator("make_zombie done, ");
-    make_pale(face);
+    const auto WIDTH = orig_face.size().width;
+    const auto HEIGHT = orig_face.size().height;
+    cv::resize(zombie_face, zombie_face,
+               cv::Size(WIDTH, HEIGHT), 0.0, 0.0, cv::INTER_LINEAR);
+    auto face_it = orig_face.begin<cv::Vec3b>();
+    auto zombie_it = zombie_face.begin<cv::Vec3b>();
+    const auto end = orig_face.end<cv::Vec3b>();
+
+    for (;face_it < end; ++face_it, ++zombie_it) {
+        int sum = 0;
+        for (int i = 0; i < 3; ++i) {
+            sum += (*zombie_it)[i];
+        }
+        if (sum >= 750)
+            continue;
+        *face_it = *zombie_it;
+     }
 }
 
 void make_pale(cv::Mat& face, double gamma)
@@ -31,4 +47,9 @@ void make_pale(cv::Mat& face, double gamma)
         p[i] = cv::saturate_cast<uchar>(pow(i / 255.0, gamma) * 255.0);
 
    cv::LUT(face, lookUpTable, face);
+}
+
+void alpha_blend()
+{
+
 }
